2e5f8139dad0b447a6390207ae68a72456327f61
deoliveira2021 <sousaedvaldo@gmail.com>
2024-02-25 13:46:54 -0300

    Alterações nos imports from django.utils.http import urlquote para from django.utils.http import quote e no from django.utils.translation import ugettext_lazy para from django.utils.translation import gettext_lazy Porque as versões anteriores foram descontinuadas.

--- usuario/models.py
+++ usuario/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.core import validators
 from django.utils import timezone
 from django.core.mail import send_mail
-from django.utils.http import urlquote
-from django.utils.translation import ugettext_lazy as _
+from django.utils.http import quote
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.conf import settings
 
